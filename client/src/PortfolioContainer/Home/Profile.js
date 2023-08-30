b4dd35e685d501d86a69ae7bc7407bb2437ee650
julissaortiz <126351385+julissaortiz@users.noreply.github.com>
2023-08-30 13:55:39 -0500

    connected my socials

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <a href="https://www.facebook.com/julissa.ortiz.9404">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="https://www.instagram.com/julissa.ortizzz/">
+              <i className="fa fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
