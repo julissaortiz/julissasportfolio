4c53dd6edade1b3df28120439455e5255a0c6e6a
julissaortiz <126351385+julissaortiz@users.noreply.github.com>
2023-08-30 16:35:42 -0500

    updated profile js , included some buttons and my resume

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Typical from "react-typical";
 
 export default function Profile() {
   return (
@@ -6,13 +7,59 @@ export default function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="https://www.facebook.com/julissa.ortiz.9404">
-              <i className="fa fa-facebook-square"></i>
-            </a>
-            <a href="https://www.instagram.com/julissa.ortizzz/">
-              <i className="fa fa-instagram"></i>
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/julissa.ortiz.9404">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="https://www.instagram.com/julissa.ortizzz/">
+                <i className="fa fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {""}
+              Hello, I'm <span className="highlighted-text">Julissa</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+            <span className="primary-text">
+              {""}
+              <h1>
+                {""}
+                <Typical
+                  loop={Infinity}
+                  steps={[
+                    "Ethusiastic Dev 🚨",
+                    4000,
+                    "Full Stack Developer 💻",
+                    4000,
+                    "MERN Stack Dev ⚙️",
+                    4000,
+                    "React Dev 📈",
+                    4000,
+                  ]}
+                />
+              </h1>
+              <span className="profile-role-tagline">
+                Ability to build applications with front and back end
+                operations.
+              </span>
+            </span>
+          </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me {""}
+            </button>
+            <a href="Resume2022(1).pdf" download="Resume2022(1).pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
             </a>
           </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
