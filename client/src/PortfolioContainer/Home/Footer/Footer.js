91c235461d081b8845b88b6ce5ad76a311eff1b0
julissaortiz <126351385+julissaortiz@users.noreply.github.com>
2023-08-30 22:15:36 -0500

    updated my about me section

---
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="footer-parent">
+        <h1 className="about-me">About Me</h1>
+        <h2 className="palabras">
+          {" "}
+          My name is Julissa Ortiz and I have just graduated from the University
+          of Minnesota TC Full Stack Webx Developement Bootcamp. I will also be
+          gaining multiple certificates through the U.S. Armed Forces
+        </h2>
+      </div>
+    </div>
+  );
+}
