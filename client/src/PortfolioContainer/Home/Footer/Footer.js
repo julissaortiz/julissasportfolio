8d38462c2d9f27650dc17f704473a02b4b59fe8c
julissaortiz <126351385+julissaortiz@users.noreply.github.com>
2023-08-30 22:34:29 -0500

    added my linked in and github

--- client/src/PortfolioContainer/Home/Footer/Footer.js
+++ client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -10,8 +10,19 @@ export default function Footer() {
           {" "}
           My name is Julissa Ortiz and I have just graduated from the University
           of Minnesota TC Full Stack Webx Developement Bootcamp. I will also be
-          gaining multiple certificates through the U.S. Armed Forces
+          gaining multiple certificates through the U.S. Armed Forces.
         </h2>
+        <h3 className="contact-me">Contact Me</h3>
+        <h4 className="palabras-2">
+          <p>Check out my GitHub profile:</p>
+          <a href="https://github.com/julissaortiz">My GitHub Profile</a>
+          <br></br>
+          <br></br>
+          <p>Check out my LinkedIn profile:</p>
+          <a href="https://www.linkedin.com/in/julissa-ortiz-46a853288/">
+            My LinkedIn Profile
+          </a>
+        </h4>
       </div>
     </div>
   );
